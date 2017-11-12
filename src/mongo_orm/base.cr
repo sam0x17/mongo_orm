fc8068353f1495262274d9017bc6389ff3f244c9
Sam Johnson <sam@durosoft.com>
2017-11-12 15:45:30 -0500

    refactor and enable Mongo::ORM::Base

--- src/mongo_orm/base.cr
+++ src/mongo_orm/base.cr
@@ -1,31 +1,38 @@
-require "./associations"
-require "./callbacks"
-require "./fields"
-require "./querying"
-require "./settings"
-require "./table"
-require "./transactions"
-require "./validators"
+#require "./associations"
+#require "./callbacks"
+#require "./fields"
+#require "./querying"
+#require "./settings"
+#require "./table"
+#require "./transactions"
+#require "./validators"
 require "./version"
 
-# Granite::ORM::Base is the base class for your model objects.
-class Granite::ORM::Base
-  include Associations
-  include Callbacks
-  include Fields
-  include Settings
-  include Table
-  include Transactions
-  include Validators
+module DB # needed for fields support
+  TYPES = [Nil, String, Bool, Int32, Int64, Float32, Float64, Time, Bytes]
+  {% begin %}
+    alias Any = Union({{*TYPES}})
+  {% end %}
+end
 
-  extend Querying
+# Mongo::ORM::Base is the base class for your model objects.
+class Mongo::ORM::Base
+  #include Associations
+  #include Callbacks
+  #include Fields
+  #include Settings
+  #include Table
+  #include Transactions
+  #include Validators
+
+  #extend Querying
 
   macro inherited
     macro finished
-      __process_table
-      __process_fields
-      __process_querying
-      __process_transactions
+      #__process_table
+      #__process_fields
+      #__process_querying
+      #__process_transactions
     end
   end
 
