02df2624b4f78a2c42679eec77b544573a21414a
Sam Johnson <sam@durosoft.com>
2017-11-25 02:13:33 -0500

    add specs for UTC time handling

--- spec/fields_spec.cr
+++ spec/fields_spec.cr
@@ -22,12 +22,29 @@ describe Mongo::ORM::Document do
       u.identifier.should eq 123_i64
     end
 
-    it "can handle Time fields" do
+    it "can handle built-in Time fields" do
       u = TestUser.new
       u.save
       u.updated_at.should_not eq nil
       u_new = TestUser.find(u._id).not_nil!
       u.updated_at.to_s.should eq u_new.updated_at.to_s
+    end
+
+    it "can handle custom Time fields" do
+      u = TestUser.new
+      u.deleted_at = Time.now.to_utc
+      u.save
+      u_new = TestUser.find(u._id).not_nil!
+      u.deleted_at.to_s.should eq u_new.deleted_at.to_s
+    end
+
+    it "should force time fields into UTC" do
+      u = TestUser.new
+      u.deleted_at = Time.now
+      u.save
+      u_new = TestUser.find(u._id).not_nil!
+      u.deleted_at.to_s.should_not eq u_new.deleted_at.to_s
+      u.deleted_at.not_nil!.to_utc.to_s.should eq u_new.deleted_at.to_s
     end
 
     it "can handle Bool fields" do
